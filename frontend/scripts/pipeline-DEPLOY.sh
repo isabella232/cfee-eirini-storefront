de615fdc78bf082dd22b46abe3e5f9d40b64882f
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-04-29 15:56:55 -0500

    fix variable names

--- frontend/scripts/pipeline-DEPLOY.sh
+++ frontend/scripts/pipeline-DEPLOY.sh
@@ -5,7 +5,7 @@ curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
 # login to IBM Cloud using the API key targeting a region and then target CFEE org and space.
 #
 ibmcloud login --apikey "$API_KEY" 
-ibmcloud target --cf-api "$CFFE_API_ENDPOINT" -o "$CFFE_ORG" -s "$CFFE_SPACE"
+ibmcloud target --cf-api "$CFEE_API_ENDPOINT" -o "$CFEE_ORG" -s "$CFEE_SPACE"
 
 ibmcloud cf push --no-start
 ibmcloud cf set-env frontend BACKEND_URL http://backend-service.default.svc.cluster.local:8080
