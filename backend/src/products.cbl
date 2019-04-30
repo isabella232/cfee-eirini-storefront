2a0a69b76ec687063ba96e017c9bd278e913a6c7
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2019-04-30 10:59:54 -0500

    more affordable products :)

--- backend/src/products.cbl
+++ backend/src/products.cbl
@@ -3,11 +3,11 @@ PROGRAM-ID.  DEMO.
 PROCEDURE DIVISION.
 DisplayPrompt.
   DISPLAY '['
-          '{ "name": "Product A", "brand": "brand", "price": 67 },'
-          '{ "name": "Product B", "brand": "brand", "price": "130" },'
-          '{ "name": "Product C", "brand": "brand", "price": "210" },'
-          '{ "name": "Product D", "brand": "brand", "price": "45" },'
-          '{ "name": "Product E", "brand": "brand", "price": "20" },'
-          '{ "name": "Product F", "brand": "brand", "price": "170" }'
+          '{ "name": "Product A", "brand": "brand", "price": "8" },'
+          '{ "name": "Product B", "brand": "brand", "price": "18" },'
+          '{ "name": "Product C", "brand": "brand", "price": "7" },'
+          '{ "name": "Product D", "brand": "brand", "price": "4" },'
+          '{ "name": "Product E", "brand": "brand", "price": "5" },'
+          '{ "name": "Product F", "brand": "brand", "price": "14" }'
           ']'
   STOP RUN.
